f147314c4f05d3e24822a79ac9154cdd60a87a13
Jeremy Yandell <jereyand@gmail.com>
2019-08-09 15:25:43 -0700

    link api for fetch movies

--- emp-app/src/CreateEmployee/index.js
+++ emp-app/src/CreateEmployee/index.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class CreateEmployee extends Component {
+    state = {
+        name: '',
+        birthDate: '',
+        department: '',
+        salary: ''
+    }
+    updateEmployee = (e)  =>  {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
     render() {
         return(
             <div>
@@ -8,19 +19,19 @@ class CreateEmployee extends Component {
                 <form>
                     <label>
                         Name:
-                        <input type="text" name="name"/>
+                        <input type="text" name="name" onChange={this.updateEmployee}/>
                     </label><br />
                     <label>
                         Birth Date:
-                        <input type="date" name="birthDate"/>
+                        <input type="date" name="birthDate" onChange={this.updateEmployee}/>
                     </label><br />
                     <label>
                         Department:
-                        <input type="text" name="department"/>
+                        <input type="text" name="department" onChange={this.updateEmployee}/>
                     </label><br />
                     <label>
                         Salary:
-                        <input type="text" name="annualSalary"/>
+                        <input type="text" name="annualSalary" onChange={this.updateEmployee}/>
                     </label><br />
                     <button type="submit">
                         Submit
--- emp-app/src/EmployeeContainer/index.js
+++ emp-app/src/EmployeeContainer/index.js
@@ -3,6 +3,29 @@ import CreateEmployee from '../CreateEmployee';
 
 
 class EmployeeContainer extends Component {
+    state = {
+        employees: []
+    }
+    componentDidMount() {
+        this.getEmployees();
+    }
+    getEmployees = async () =>  {
+        try {
+            const responseGetEmployees = await fetch('http://localhost:9000/api/v1/employee');
+            console.log(responseGetEmployees, 'responseGetEmployees in getEmployees app.js');
+            if(responseGetEmployees.status !== 200){
+                throw Error('404 from server');
+            }
+            const employeesResponse = await responseGetEmployees.json();
+            console.log(employeesResponse, 'employees response after json');
+            this.setState({
+                employees: [...employeesResponse.data]
+            });
+        } catch(err) {
+            console.log(err, 'err in getEmployees');
+            return err;
+        }
+    }
     render() {
         return(
             <div>
